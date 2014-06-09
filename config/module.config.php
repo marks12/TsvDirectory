bb9bf08ab29fbbe09106f937ee22e4c1ea9d78b9
tsarevnikov <tsarevnikov@srv-web-02.entc.nsk>
2014-06-09 11:37:11 +0700

    Doctrine require, navigation, router added

--- config/module.config.php
+++ config/module.config.php
@@ -10,39 +10,43 @@ return array(
     ),
     'router' => array(
         'routes' => array(
-            'tsv-directory' => array(
-                'type'    => 'Literal',
-                'options' => array(
-                    // Change this to something specific to your module
-                    'route'    => '/tsvDirectory',
-                    'defaults' => array(
-                        // Change this value to reflect the namespace in which
-                        // the controllers for your module are found
-                        '__NAMESPACE__' => 'TsvDirectory\Controller',
-                        'controller'    => 'TsvDirectory',
-                        'action'        => 'index',
-                    ),
-                ),
-                'may_terminate' => true,
-                'child_routes' => array(
-                    // This route is a sane default when developing a module;
-                    // as you solidify the routes for your module, however,
-                    // you may want to remove it and replace it with more
-                    // specific routes.
-                    'default' => array(
-                        'type'    => 'Segment',
-                        'options' => array(
-                            'route'    => '/[:controller[/:action]]',
-                            'constraints' => array(
-                                'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
-                                'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
-                            ),
-                            'defaults' => array(
-                            ),
-                        ),
-                    ),
-                ),
-            ),
+    		'zfcadmin' => array(
+				'child_routes' => array(
+					'tsv-directory' => array(
+						'type'    => 'Literal',
+						'options' => array(
+							// Change this to something specific to your module
+							'route'    => '/tsvDirectory',
+							'defaults' => array(
+								// Change this value to reflect the namespace in which
+								// the controllers for your module are found
+									'__NAMESPACE__' => 'TsvDirectory\Controller',
+									'controller'    => 'TsvDirectory',
+									'action'        => 'index',
+							),
+						),
+						'may_terminate' => true,
+						'child_routes' => array(
+							// This route is a sane default when developing a module;
+							// as you solidify the routes for your module, however,
+							// you may want to remove it and replace it with more
+							// specific routes.
+							'default' => array(
+								'type'    => 'Segment',
+								'options' => array(
+										'route'    => '/[:controller[/:action]]',
+										'constraints' => array(
+												'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
+												'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
+										),
+									'defaults' => array(
+									),
+								),
+							),
+						),
+					),
+    			),
+    		),
         ),
     ),
     'view_manager' => array(
@@ -50,4 +54,12 @@ return array(
             'TsvDirectory' => __DIR__ . '/../view',
         ),
     ),
+	'navigation' => array(
+			'admin' => array(
+				'directory' => array(
+						'label' => 'Управление данными',
+						'route' => 'zfcadmin/tsv-directory',
+				),
+			),
+	),
 );
