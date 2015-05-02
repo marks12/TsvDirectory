5e866b1099d4aae8c21a08d894a15b73a4617717
marks12 <webts@bk.ru>
2015-05-02 21:50:30 +0700

    f1

--- src/TsvDirectory/View/Helper/TgetFForm.php
+++ src/TsvDirectory/View/Helper/TgetFForm.php
@@ -64,12 +64,50 @@ class TgetFForm extends AbstractHelper
 			
 			$target_params = $em->getClassMetadata($table_params->associationMappings[$field_name]['targetEntity']);
 			
-			var_dump($target_params);
+			$table_config = $em->getRepository('TsvDirectory\Entity\TsvTable')->findOneBy(array("entity"=>$table_params->name));
 			
-			if(isset($target_params->table) && isset($target_params->table['options']) && isset($target_params->table['options']['comment']))
-				return $target_params->table['options']['comment'];
+			$linked_fields = $table_config->__get('linked_fields');
+
+			$linked_fields = explode(";", $linked_fields);
+			
+			if(count($linked_fields))
+				unset($linked_fields[count($linked_fields)-1]);
+
+			$linked_error = true;
+			foreach ($linked_fields as $k=>$v)
+			{
+				if(strstr($v, $field_name))
+				{
+					$linked_error = false;
+					$select_fields[] = explode(":", substr($v, 1))[1];
+				}
+				
+			}
+
+
+			if($linked_error)
+			{
+				
+				return '<div class="panel panel-danger">
+						  <div class="panel-body">
+						    В настоящее время не указаны <strong>связи</strong> для таблицы 
+							<code>'.$target_params->table['options']['comment'].'</code>
+							<a href="'.$this->view->url('zfcadmin/tsv-directory/table',array("action"=>"configure","id"=>$table_config->__get('id'))).'">Настроить</a>
+						  </div>
+						</div>';
+			}
 			else 
-				return 'Наименование не установлено';
+			{
+				$title = $target_params->table['options']['comment'];
+				
+// 				var_dump($select_fields);
+				
+				return '
+				<div class="form-group">
+					<label for="tt-'.$target_params->table['name'].'">'.$target_params->table['options']['comment'].'</label>
+				    <input type="text" class="form-control" id="tt-min_land_size_img" placeholder="Введите данные для поиска по таблице '.$target_params->table['options']['comment'].'">
+				</div>';
+			}
 		}
 		
 // 		$getEM =   $this->sm->getServiceLocator()->get('TsvDirectory\Service\GetEM');
